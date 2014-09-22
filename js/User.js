a7485e01884e5b25f6e15f23a53b2a10d03ee204
Gediminas Bivainis <gediminas@bivainis.com>
2014-09-22 21:52:45 +0200

    auto increment user id on sign up

--- js/User.js
+++ js/User.js
@@ -5,23 +5,43 @@ var User = (function(){
 
     var _userTable;
 
-    var make = function(fields) {
+    var _generateUserId = function (data) {
+
+        var newId;
+
+        if (data.length > 0){
+            var lastInsert = data.splice(-1),
+                lastId = lastInsert[0].id;
+
+            newId = lastId+1;
+        } else {
+            newId = 1;
+        }
+
+        return newId;
+    };
+    var _getGravatar = function (email) {
+        return 'ajdfhasdfjlhjlsdfk';
+    };
+
+    var make = function(formData) {
 
         // todo: check if there are any users in local storage
         // if no users found, create with id 1
         _userTable = localStorage.keabookUsers ? JSON.parse(localStorage.keabookUsers) : [];
 
+        console.log();
         _userTable.push({
-            "id" : 1,
-            "name" : "Gediminas",
-            "surname" : "Bivainis",
-            "email" : fields.email,
-            "password" : "{ cryptojs hash }",
+            "id" : _generateUserId(_userTable),
+            "name" : undefined,
+            "surname" : undefined,
+            "email" : formData.email,
+            "password" : formData.password,
             "loggedIn" : false,
             "loginAt" : "{ unix timestamp }",
             "rememberMe" : 0,
-            "type" : 1,
-            "gravatar" : "{ hash }",
+            "type" : 0, // 0 - regular, 1 - admin
+            "gravatar" : _getGravatar(formData.email),
             "createdAt" : "{ unix timestamp }",
             "updatedAt" : "{ unix timestamp }"
         });
